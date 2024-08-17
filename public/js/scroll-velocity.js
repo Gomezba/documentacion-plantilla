9b5197de8e191c3eb1082c0877bb6f068e484e4d
gomezba <joel_gomezba@hotmail.com>
2024-08-16 21:02:54 -0600

    Completed template documentation

---
+++ public/js/scroll-velocity.js
@@ -0,0 +1,18 @@
+export const scrollVelocity = () => {
+	document.addEventListener(
+		'wheel',
+		function (event) {
+			event.preventDefault() // Evitar el comportamiento predeterminado de scroll
+
+			let scrollAmount = 30 // Controla la cantidad de scroll en píxeles
+			if (event.deltaY > 0) {
+				// Scroll hacia abajo
+				window.scrollBy(0, scrollAmount)
+			} else {
+				// Scroll hacia arriba
+				window.scrollBy(0, -scrollAmount)
+			}
+		},
+		{ passive: false }
+	)
+}
